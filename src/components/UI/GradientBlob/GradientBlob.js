e465745f0be20d673aea34921c223b65bb4fb15c
deepak <deepakceo0@gmail.com>
2023-02-07 19:09:04 +0530

    fixed gradient white color issue, and added scrollbar

--- src/components/UI/GradientBlob/GradientBlob.js
+++ src/components/UI/GradientBlob/GradientBlob.js
@@ -3,8 +3,8 @@ import "./GradientBlob.css";
 
 const GradientBlob = () => {
 
-    let num1 = Math.floor(Math.random()*16777215).toString(16);
-    let num2 = Math.floor(Math.random()*16777215).toString(16);
+    let num1 = Math.floor(Math.random()*16777215).toString(16).padStart(6, 0);
+    let num2 = Math.floor(Math.random()*16777215).toString(16).padStart(6, 0);
     let grad = `conic-gradient(from 265deg at 50% 50%, hsl(222deg 39% 94%) 45deg, #${num1} 226deg, #${num2} 323deg)`;
 
     return (
