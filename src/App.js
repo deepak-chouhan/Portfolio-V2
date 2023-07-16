c93ef23b426dcf5a54cfc13dcac4a2ee3bd33986
deepak-chouhan <deepakceo0@gmail.com>
2023-07-16 22:05:14 +0530

    Added Fractal Noise

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes} from 'react-router-dom';
 import './App.css';
 import Archive from './pages/Archive/Archive';
 import Index from './pages/Index/Index';
+import FractalNoise from "./components/UI/FractalNoise/FractalNoise";
 
 
 function App() {
   return (
     <div className="App">
+      <FractalNoise />
       <Routes>
           <Route path='/' element={<Index/>}></Route>
           <Route path='/archive' element={<Archive/>}></Route>
