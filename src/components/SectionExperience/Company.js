77090468c9fd13863429f943a9532e70149094e8
deepak-chouhan <deepakceo0@gmail.com>
2024-01-07 13:05:02 +0530

    Update Experience Section

--- src/components/SectionExperience/Company.js
+++ src/components/SectionExperience/Company.js
@@ -5,13 +5,17 @@ import Skills from "../UI/Technologies/Skills";
 const Company = (props) => {
     return (
         <div className="company">
-            <a className="company__name btn__underline" href={props.experience.company_link} target="opener">{props.experience.company}</a>
+            <a className="company__name" href={props.experience.company_link} target="opener">{props.experience.company}</a>
             <div className="company__experience">
                 <div className="company__roles">
                     {props.experience.roles.map(role => 
                         <div className="company__role">
                             <div className="company__role__name">{role.name}</div>
-                            <div className="company__role__duration">{role.from} — {role.to}</div>
+                            <div className="company__role__duration">
+                                <div className="company__role__duration__start">{role.from}</div>
+                                <div className="company__role__duration__end"> — {role.to}</div>
+                                {/* {role.from} — {role.to} */}
+                            </div>
                         </div>
                     )}
                 </div>
